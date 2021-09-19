a46e688f7a098e567577f250be5570c7077ed238
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-20 01:46:13 +0530

    feat: add base balance command

---
+++ src/commands/balance.ts
@@ -0,0 +1,23 @@
+import { Command, flags } from '@oclif/command'
+
+export default class Balance extends Command {
+	static description = `get the balance for ur Wallet's Address`
+
+	static examples = [
+		`$ hwallet balance\n Hey there 👋 Your Balance is xyz HC!`,
+	]
+
+	static flags = {
+		help: flags.help({ char: 'h' }),
+	}
+
+	static usage = "balance"
+
+	static args = [{ name: 'file' }]
+
+	async run() {
+		const { args, flags } = this.parse(Balance)
+		console.log(args)
+		console.log(`Hey there 👋 Your Balance is xyz HC!`,)
+	}
+}
